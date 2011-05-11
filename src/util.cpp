670cbac9a24cf83da9e4489dacb495a9e6b364ff
Pedro Silva <pedro.alex.silva@gmail.com>
2011-05-11 12:07:53 +0100

    Added util.{h,cpp} which were missing from previous commit

---
+++ src/util.cpp
@@ -0,0 +1,38 @@
+#include "util.h"
+#include <QLayoutItem>
+#include <QWidget>
+#include <QDebug>
+
+namespace Util {
+    void removeLayoutChildren(QLayout* layout, int fromIndex)
+    {
+        QLayoutItem *child;
+        QWidget *childWidget;
+
+        while ((child = layout->takeAt(fromIndex)) != 0)
+        {            
+            childWidget = child->widget();            
+            delete childWidget;
+            delete child;
+        }
+    }
+
+    vector<string> split(const string& s, char c)
+    {
+        string::size_type i = 0;
+        string::size_type j = s.find(c);
+        vector<string> v;
+    
+        while (j != string::npos) {   
+            v.push_back(s.substr(i, j-i));
+            i = ++j;
+            j = s.find(c, j);
+        
+            if (j == string::npos) {
+                v.push_back(s.substr(i, s.length( )));
+            }
+        }
+
+        return v;
+    }
+}
---
+++ src/util.h
@@ -0,0 +1,14 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <QLayout>
+#include <vector>
+#include <string>
+using namespace std;
+
+namespace Util {
+    void removeLayoutChildren(QLayout* layout, int fromIndex);
+    vector<string> split(const string& s, char c);
+}
+
+#endif //UTIL_H
